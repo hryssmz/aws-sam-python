755f99bc985ab14543296801f23ca1086823b3fa
Hiroyasu Shimizu <hryssmz@outlook.com>
2023-03-19 05:07:07 +0900

    update source

--- cicd/hello_world_app/test-unit.sh
+++ cicd/hello_world_app/test-unit.sh
@@ -2,10 +2,11 @@
 pytest_lambda() {
     pytest \
         --cov-report=term \
-        --cov-report="html:htmlcov" \
-        --cov-report="xml:coverage.xml" \
+        --cov-report="html:tests/report/htmlcov/$1" \
+        --cov-report="xml:tests/report/clover/$1.xml" \
         --cov="functions/$1" \
         --override-ini="pythonpath=functions/$1 ." \
+        --junitxml="tests/report/junit/$1.xml" \
         "tests/unit/$1"
 }
 
