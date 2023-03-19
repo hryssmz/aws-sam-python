190f94d3c15c93a820a751092a81ddeb41aac75f
Hiroyasu Shimizu <hryssmz@outlook.com>
2023-03-19 13:35:49 +0900

    update source

--- cicd/hello_world_app/test-unit.sh
+++ cicd/hello_world_app/test-unit.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 pytest_lambda() {
+    mkdir -p "tests/$1/report/log/$2"
     pytest \
+        --override-ini="pythonpath=functions/$1/$2 ." \
+        --log-file-level=INFO \
+        --log-file="tests/$1/report/log/$2/test_unit.log" \
+        --junitxml="tests/$1/report/junit/$2.xml" \
         --cov-report=term \
         --cov-report="html:tests/$1/report/htmlcov/$2" \
         --cov-report="xml:tests/$1/report/clover/$2.xml" \
         --cov="functions/$1/$2" \
-        --override-ini="pythonpath=functions/$1/$2 ." \
-        --junitxml="tests/$1/report/junit/$2.xml" \
-        "tests/$1/$2/test_unit.py"
+        "tests/$1/$2/test_unit.py" |
+            tee "tests/$1/report/log/$2/pytest.log"
 }
 
 pytest_hello_world_app() {
